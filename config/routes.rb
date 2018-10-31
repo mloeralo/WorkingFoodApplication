d1335f7157a5d429ea27eb1a4400fd78f2dc2819
mloeralo <maryelena2803@hotmail.com>
2018-10-31 16:13:02 -0400

    Sign Up working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'units/index'
   get 'categories/index'
   #get 'recipes/index'
-  get 'users/new'
+
   get 'static_pages/home'
   get 'static_pages/favorites'
   get 'units', to: 'units#index', as: 'units'
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
   resources :categories
+  resources :users
   root 'ingredients#index'
   #root 'static_pages#home'
 
