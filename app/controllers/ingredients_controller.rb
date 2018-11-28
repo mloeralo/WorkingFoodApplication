fd8531ae73daf82dab784e148dfcb782cad27904
mloeralo <maryelena2803@hotmail.com>
2018-11-28 15:52:40 -0500

    More Styling

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,7 +2,7 @@ class IngredientsController < ApplicationController
   def index
     @ingredients=Ingredient.all
     @favorites= Favorite.all
-    @search_term = params[:looking_for] || 'chicken'
+    @search_term = params[:looking_for] || 'cheese'
     @recipes = Recipe.for(@search_term)
   end
 end
